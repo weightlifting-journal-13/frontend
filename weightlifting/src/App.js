8343840c43fe1432d4bb1e36a58122db5603e13d
toddmurphy <toddmurphy93@gmail.com>
2020-01-03 03:53:51 -0400

    added render props for Login, CreatePlan, MyPlans

--- weightlifting/src/App.js
+++ weightlifting/src/App.js
@@ -11,10 +11,17 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <Route exact path='/' component={Login} />
+      {/* <Route exact path='/' component={Login} /> */}
+      <Route 
+        exact path='/' 
+        render={props => <Login {...props} />} />
       <PrivateRoute  exact path='/Dashboard' component={Dashboard}/>
-      <Route exact path='/CreatePlan' component={CreatePlan} />
-      <Route exact path='/MyPlans' component={MyPlans} />
+      <Route 
+        exact path='/CreatePlan' 
+        render={props => <CreatePlan {...props} />} />
+      <Route 
+        exact path='/MyPlans' 
+        render={props => <MyPlans {...props} />} />
     </div>
   );
 }
--- weightlifting/src/components/Login.js
+++ weightlifting/src/components/Login.js
@@ -1,47 +1,71 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { axiosWithAuth } from '../utils/axiosWithAuth';
 
 
-const Login = () => {
+const Login = (props) => {
     // make post request to receive token from api
     //handle the token, navigate to 'Dashboard' component page
 
-
     //setup useState to store inital state/data
-
+    const [credentials, setCredentials] = useState({
+        username: '',
+        password: '',
+        id: Date.now()
+    })
 
     //handleInputChanges
-
+    const handleInputChanges = (e) => {
+        setCredentials({
+            ...credentials,
+            [e.target.name]: e.target.value
+        })
+        console.log(credentials)
+    }
 
     //handleLoginSubmit
     //use axiosWithAuth --> all done inside handleLoginSubmit
     //successful post --> props.history.push('/Dashboard')
+    const handleLoginSubmit = (e) => {
+        e.preventDefault();
 
+        //axiosWithAuth to login
+        axiosWithAuth()
+            .post('/login', credentials)
+            .then(response => {
+                console.log(response)
+                localStorage.setItem('token', response)
+
+                //reset values back to empty strings
+                setCredentials({
+                    username: '',
+                    password: ''
+                })
+
+                //automatically redirect from Login to --> Dashboard
+                props.history.push('/Dashboard')
+            })
+            .catch(error => {
+                console.log('Sorry, login credentials not valid.', error)
+            })
+    }
 
     return (
         <div>
             <h1>Please login</h1>
-            <form>
+            <form onSubmit={handleLoginSubmit}>
                 <input
                     type='text'
                     name='username'
-                    placeholder='Username'
-                //value={}
-                //onChange={}
-                />
-                <input
-                    type='email'
-                    name='email'
-                    placeholder='Email'
-                //value={}
-                //onChange={}
+                    placeholder='Email (username)'
+                    value={credentials.username}
+                    onChange={handleInputChanges}
                 />
                 <input
                     type='password'
                     name='password'
                     placeholder='Password'
-                //value={}
-                //onChange={}
+                    value={credentials.password}
+                    onChange={handleInputChanges}
                 />
                 <button type='submit'>Login</button>
             </form>
