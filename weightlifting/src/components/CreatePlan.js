e78eb90d59bcdc8820442c6b6462c85187cc1d4f
toddmurphy <toddmurphy93@gmail.com>
2020-01-04 15:42:41 -0400

    finally got checkboxes to map and label

--- weightlifting/src/components/CreatePlan.js
+++ weightlifting/src/components/CreatePlan.js
@@ -1,45 +1,33 @@
-import React, {useState, useEffect} from 'react';
-import {data} from '../data';
+import React, { useState, useEffect } from 'react';
+import { data } from '../data';
 
 const CreatePlan = () => {
-     const [workoutPlan, setWorkoutPlan] = useState(data)
+    const [workoutPlan, setWorkoutPlan] = useState(data)
     // 0) Prereq. Given the "bodypart", such as "chest"
     // const bodypart = "legs"; // from set state of the <select />
 
     //set up useEffect --> watch two things: 1. bodpyarts, 2. exercises
 
-    const result = Object.entries(workoutPlan)
-    const mappedWorkoutPlan = result.map(item => item)
-    console.log(mappedWorkoutPlan)
-    
-   //NATE:
-/**
- * data  -state (check)
- * map through and create a list that has check box that has a helper function that changes your state.
- * 
- *     
-        data = {
-             chest:{
-                 isSelected:false,
-                 exercises:{
-                     flat_Bench_Press:{
-                         isSelected:false,
-                         exercisename: "Flat Bench Press"
-                     }
-                 }
-             }
-         }
- * 
+    const result = Object.values(workoutPlan)
+    console.log(result)
 
- handleInputBodyPart --> 
- setWorkoutPlan({
-    state[e.target.name]= {
-        ...state[e.target.name]
-        isClicked:!state[e.target.name]
-    }
- })
-  
- */
+       //NATE:
+    /**
+     * data  -state (check)
+     * map through and create a list that has check box 
+     * that has a helper function that changes your state.
+     * 
+    
+     
+     handleInputBodyPart --> 
+     setWorkoutPlan({
+         state[e.target.name]= {
+             ...state[e.target.name]
+             isSelected:!state[e.target.name]
+            }
+        })
+        
+        */
     return (
         <div>
             <h1>Create new plan component</h1>
@@ -59,109 +47,47 @@ const CreatePlan = () => {
                 //value={}
                 //onChange={}
                 />
-                <h3>Select your body part</h3>
-                    <label>Chest</label>
-                    <input 
-                        type='checkbox'
-                        name='chest'
-                        //value={}
-                    />
-                    <label>Legs</label>
-                    <input 
-                        type='checkbox'
-                        name='legs'
-                        //value={}
-                    />
-                    <label>Shoulders</label>
-                    <input 
-                        type='checkbox'
-                        name='shoulders'
-                        //value={}
-                    />
-                    <label>Back</label>
-                    <input 
-                        type='checkbox'
-                        name='back'
-                        //value={}
-                    />
-                    <label>Triceps</label>
-                    <input 
-                        type='checkbox'
-                        name='triceps'
-                        //value={}
-                    />
-                    <label>Biceps</label>
-                    <input 
-                        type='checkbox'
-                        name='biceps'
-                        //value={}
-                    />
-                    <label>Abs/Core</label>
-                    <input 
-                        type='checkbox'
-                        name='core'
-                        //value={}
-                    />
-                    <label>Cardio</label>
-                    <input 
-                        type='checkbox'
-                        name='cardio'
-                        //value={}
-                    />
-                    <label>Circuit</label>
-                    <input 
-                        type='checkbox'
-                        name='circuits'
-                        //value={}
-                    />
-                    <label>Functional Training</label>
-                    <input 
-                        type='checkbox'
-                        name='training'
-                        //value={}
-                    />
-                    <label>Stretching</label>
-                    <input 
-                        type='checkbox'
-                        name='stretching'
-                        //value={}
-                    />
-                    <label>Bodyweight</label>
-                    <input 
-                        type='checkbox'
-                        name='bodyweight'
-                        //value={}
-                    />
-                <h4>Select your exercises(from data file)</h4>
+
+                <h4>Select your exercises</h4>
+                
+                {result.map((item, index) => {
+                return (
+                    <>
+                    <input key={index} type='checkbox'/>
+                    <label>{item.bodypart}</label>
+                    </>
+                    )
+                })}
+
                 <h3>Fill out exercises reps</h3>
-    
+
                 <label>Exercise</label>
-                <input 
+                <input
                     type='text'
                     name='exercise'
                     placeholder='Bench press(autopopulate)'
                 />
                 <label>Number of sets</label>
-                <input 
+                <input
                     type='number'
                     name='sets'
                     placeholder='Number'
                 />
                 <label>Number of reps</label>
-                <input 
+                <input
                     type='number'
                     name='sets'
                     placeholder='Number'
                 />
                 <label>lbs-optional</label>
-                <input 
+                <input
                     type='number'
                     name='reps'
                     placeholder='weight'
                 />
                 <button>Create plan</button>
             </form>
-        </div>
+        </div >
     );
 }
 
--- weightlifting/src/data.js
+++ weightlifting/src/data.js
@@ -1,96 +1,125 @@
 export const data = {
-    chest:{
-        isSelected:false,
-        exercises:{
-            flat_Bench_Press:{
-                isSelected:false,
+    chest: {
+        bodypart: 'chest',
+        isSelected: false,
+        exercises: {
+            flat_Bench_Press: {
+                isSelected: false,
                 exercisename: "Flat Bench Press"
             },
-            incline_Bench_Press:{
-                isSelected:false,
+            incline_Bench_Press: {
+                isSelected: false,
                 exercisename: "Incline Bench Press"
             },
-            decline_Bench_Press:{
-                isSelected:false,
+            decline_Bench_Press: {
+                isSelected: false,
                 exercisename: "Decline Bench Press"
             },
-            push_Ups:{
-                isSelected:false,
+            push_Ups: {
+                isSelected: false,
                 exercisename: "Push ups"
             },
-            flat_Dumbell_Press:{
-                isSelected:false,
+            flat_Dumbell_Press: {
+                isSelected: false,
                 exercisename: "Flat Dumbell Press"
             },
-            incline_Dumbell_Press:{
-                isSelected:false,
+            incline_Dumbell_Press: {
+                isSelected: false,
                 exercisename: "Incline Bench Press"
             },
-            dips:{
-                isSelected:false,
+            dips: {
+                isSelected: false,
                 exercisename: "Dips"
             },
-            cable_Cross_Overs:{
-                isSelected:false,
+            cable_Cross_Overs: {
+                isSelected: false,
                 exercisename: "Cable Crossovers"
             },
         }
     },
-    legs:{
-        isSelected:false,
-        exercises:{
-            squats:{
-                isSelected:false,
+    legs: {
+        bodypart: 'legs',
+        isSelected: false,
+        exercises: {
+            squats: {
+                isSelected: false,
                 exercisename: "Squats"
             },
-            lunges:{
-                isSelected:false,
+            lunges: {
+                isSelected: false,
                 exercisename: "Lunges"
             },
-            deadlifts:{
-                isSelected:false,
+            deadlifts: {
+                isSelected: false,
                 exercisename: "Deadlifts"
             },
-            leg_Curls:{
-                isSelected:false,
+            leg_Curls: {
+                isSelected: false,
                 exercisename: "Leg Curls"
             },
-            hamstring_Curls:{
-                isSelected:false,
+            hamstring_Curls: {
+                isSelected: false,
                 exercisename: "Hamstring Curls"
             },
-            sumo_Squat:{
-                isSelected:false,
+            sumo_Squat: {
+                isSelected: false,
                 exercisename: "Sumo Squat"
             },
-            calf_Raises:{
-                isSelected:false,
+            calf_Raises: {
+                isSelected: false,
                 exercisename: "Calf Raises"
             },
         }
     },
-    shoulders:{
-        isSelected:false,
-        exercises:{
-            seated_Press:{
-                isSelected:false,
+    shoulders: {
+        bodypart: 'shoulders',
+        isSelected: false,
+        exercises: {
+            seated_Press: {
+                isSelected: false,
                 exercisename: "Seated Press"
             },
-            upright_Rows:{
-                isSelected:false,
+            upright_Rows: {
+                isSelected: false,
                 exercisename: "UpRight Rows"
             },
-            cable_Raises:{
-                isSelected:false,
+            cable_Raises: {
+                isSelected: false,
                 exercisename: "Cable Raises"
             },
-            side_Raises:{
-                isSelected:false,
+            side_Raises: {
+                isSelected: false,
                 exercisename: "Side Raises"
             },
-            kettle_Bell_Swings:{
-                isSelected:false,
+            kettle_Bell_Swings: {
+                isSelected: false,
                 exercisename: "Kettle Bell Swings"
+            },
+        }
+    },
+    back: {
+        bodypart: 'back',
+        isSelected: false,
+        exercises: {
+            seated_Pull_Downs: {
+                isSelected: false,
+                exercisename: "Seated Lat Pull Downs"
+            },
+            seated_Rows: {
+                isSelected: false,
+                exercisename: "Seated Rows"
+            },
+            dumbell_Rows: {
+                isSelected: false,
+                exercisename: "Dumbell Rows"
+            },
+            pull_Ups: {
+                isSelected: false,
+                exercisename: "Standing Pull Ups"
+            },
+            lower_Back_Raises: {
+                isSelected: false,
+                exercisename: "Lower Back Raises"
             },
         }
     },
