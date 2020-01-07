904ce20f2d987bc9c5cc13bc840fab2f3a901a30
toddmurphy <toddmurphy93@gmail.com>
2020-01-07 16:55:46 -0400

    added exercise data as dropdown to select, started EditPlan functionality

--- weightlifting/src/components/Dashboard.js
+++ weightlifting/src/components/Dashboard.js
@@ -1,11 +1,19 @@
 import React from 'react';
 
-const Dashboard = () => {
-    return ( 
+const Dashboard = (props) => {
+
+    const editWorkoutPlan = (event) => {
+        event.preventDefault();
+
+        props.history.push(`/EditPlan/id`)
+    }
+
+    return (
         <div>
             <h1>Dashboard component</h1>
+            <button onClick={editWorkoutPlan} >Edit plan</button>
         </div>
-     );
+    );
 }
- 
+
 export default Dashboard;
--- weightlifting/src/components/EditWorkoutPlan.js
+++ weightlifting/src/components/EditWorkoutPlan.js
@@ -30,16 +30,6 @@ const TextInput = styled.input`
     border: 2px solid lightgrey;
 `
 
-const SelectInput = styled.select`
-    margin: 0 1%;
-    height: 40px;
-    width: 100%;
-    border-radius: 5px;
-    font-size: 1rem;
-    padding: 0 2%;
-    border: 2px solid lightgrey;
-`
-
 const WorkoutPlanInput = styled.input`
     width: 40%;
     height: 45px;
@@ -125,7 +115,7 @@ const LabelStyle = styled.label`
     text-align: left;
 `
 
-const NewPlan = (props) => {
+const EditWorkoutPlan = (props) => {
     const [workoutPlan, setWorkoutPlan] = useState({
         workoutplan: '',
         workoutdescription: '',
@@ -146,6 +136,9 @@ const NewPlan = (props) => {
     console.log(result)
     console.log(formData, 'FormData Todd is here')
     console.log("WORKOUT PLAN IS NOW ", workoutPlan);
+
+    //setup useEffect to get a specific 'workoutplan' from the api using a dynamic ID --> ex: /api/users/workoutplan/6
+
 
     const addToPlan = (event) => {
         event.persist();
@@ -199,7 +192,7 @@ const NewPlan = (props) => {
         e.stopPropagation()
         //axiosWithAuth to post new/add new workout
         axiosWithAuth()
-            .post('/', workoutPlan)
+            .put(`/users/workoutplan/${props.match.params.id}`, workoutPlan)
             .then(response => {
                 console.log(response)
                 setFormData(response)
@@ -221,12 +214,12 @@ const NewPlan = (props) => {
                 props.history.push('/MyPlans')
             })
             .catch(error => {
-                console.log('Sorry, workout plan not created', error)
+                console.log('Sorry, your workout plan not updated', error)
             })
     }
     return (
         <div>
-            <h1>New plan component</h1>
+            <h1>Edit your workout plan --> component</h1>
             <form onSubmit={handleOnSubmitForm} >
                 <div>
                     <WorkoutPlanInput
@@ -246,123 +239,16 @@ const NewPlan = (props) => {
                 </div>
                 <div>
                     <FormWrapper>
-                        <h1>Add your custom exercises to your plan</h1>
+                        <h1>Update your custom exercises</h1>
                         <FormContainer>
                             <LabelStyle>Exercise name</LabelStyle>
-                            {/* <TextInput
+                            <TextInput
                                 type='text'
                                 name='exercise'
                                 placeholder='Exercise'
                                 value={formData.exercise}
                                 onChange={handleInputChanges}
-                            /> */}
-                            {/* Add select options here for workout list */}
-                            <SelectInput>
-                                <option name='chest' selected disabled>Select an exercise</option>
-                                {/* chest */}
-                                <option name='chest' disabled>CHEST</option>
-                                <option name='flatebenchpress'>Flat Bench Press</option>
-                                <option name='inclinebenchpress'>Incline Bench Press</option>
-                                <option name='declinebenchpress'>Decline Bench Press</option>
-                                <option name='pushups'>Push Ups</option>
-                                <option name='flatdumbbellpress'>Flat Dumbbell Press</option>
-                                <option name='dips'>Dips</option>
-                                <option name='cablecrossovers'>Cable CrossOvers</option>
-                                <option name='dumbbellflys'>Dumbbell Flys</option>
-                                {/* legs */}
-                                <option name='legs' disabled>LEGS</option>
-                                <option name='squat'>Squat</option>
-                                <option name='deadlifts'>Deadlifts</option>
-                                <option name='lunges'>Lunges</option>
-                                <option name='legcurls'>Leg Curls</option>
-                                <option name='hamstringcurls'>Hamstring Curls</option>
-                                <option name='calfraises'>Calf Raises</option>
-                                <option name='hungariansquat'>Hungarian Squat</option>
-                                <option name='sumosquat'>Sumo Squat</option>
-                                {/* shoulders */}
-                                <option name='shoulders' disabled>SHOULDERS</option>
-                                <option name='seatedpress'>Seated Press</option>
-                                <option name='uprightrow'>Upright Row</option>
-                                <option name='cableraises'>Cable Raises</option>
-                                <option name='sideraises'>Side Raises</option>
-                                <option name='kettlebellswing'>Kettle Bell Swing</option>
-                                <option name='barbelloverheadpress'>Barbell Overhead Press</option>
-                                {/* back */}
-                                <option name='back' disabled>BACK</option>
-                                <option name='latpulldowns'>Lat Pull Downs</option>
-                                <option name='seatedrow'>Seated Row</option>
-                                <option name='bentoverdumbbellrow'>Bent Over Dumbbell Row</option>
-                                <option name='lowerbackraises'>Lower Back Raises</option>
-                                <option name='standingpullups'>Standing Pull Ups</option>
-                                <option name='goodmorning'>Good Morning</option>
-                                <option name='widegrippullups'>Wide Grip Pullups</option>
-                                <option name='shrugs'>Shrugs</option>
-                                <option name='dumbbelldeadrow'>Dumbbell Dead Row</option>
-                                <option name='barbellrow'>BarBell Row</option>
-                                {/* triceps */}
-                                <option name='triceps' disabled>TRICEPS</option>
-                                <option name='standingcableextensions'>Standing Cable Extensions</option>
-                                <option name='skullcrusher'>Skull Crusher</option>
-                                <option name='bentoverdumbbellextension'>Bent Over Dumbbell Extensions</option>
-                                <option name='dips'>Dips</option>
-                                <option name='weightedpushup'>Weighted Pushup</option>
-                                <option name='closegrippinpress'>Close Grip Pin Press</option>
-                                <option name='triceppushaways'>Tricep Pushaways</option>
-                                <option name='dragpushdowns'>Drag Pushdowns</option>
-                                {/* biceps */}
-                                <option name='biceps' disabled>BICEPS</option>
-                                <option name='dumbbellcurls'>Dumbbell Curls</option>
-                                <option name='preachercurls'>Preacher Curls</option>
-                                <option name='hammercurls'>Hammer Curls</option>
-                                <option name='weightedchinups'>Weighted Chin Ups</option>
-                                {/* abs/core */}
-                                <option name='abscore' disabled>ABS/CORE</option>
-                                <option name='situps'>Sit Ups</option>
-                                <option name='declinesitups'>Decline Sit Ups</option>
-                                <option name='dragonflags'>Dragon Flags</option>
-                                <option name='bicyclecrunches'>Bicycle Crunches</option>
-                                <option name='russiantwist'>Russian Twist</option>
-                                <option name='hanginglegraises'>Hanging Leg Raises</option>
-                                <option name='hangingkneeraises'>Hanging Knee Raises</option>
-                                <option name='declinereversecrunches'>Decline Reverse Crunches</option>
-                                {/* stretches */}
-                                <option name='stretches' disabled>STRETCHES</option>
-                                <option name='standinghamstringstretch'>Standing Hamstring Stretch</option>
-                                <option name='standingquadstretch'>Standing Quad Stretch</option>
-                                <option name='piriformisstretch'>Piriformis Stretch</option>
-                                <option name='lungewithspinaltwist'>Lunge With Spinal Twist</option>
-                                <option name='tricepsstretch'>Triceps Stretch</option>
-                                <option name='figurefourstretch'>Figure Four Stretch</option>
-                                <option name='frogstretch'>Frog Stretch</option>
-                                <option name='butterflystretch'>Butterfly Stretch</option>
-                                <option name='sidebendstretch'>Side Bend Stretch</option>
-                                <option name='lunginghipflexorstretch'>Lunging Hip Flexor Stretch</option>
-                                <option name='kneetocheststretch'>Knee To Chest Stretch</option>
-                                {/* functional */}
-                                <option name='functional' disabled>FUNCTIONAL TRAINING</option>
-                                <option name='sledpush'>Sled Push</option>
-                                <option name='tireflips'>Tire Flips</option>
-                                <option name='boxjumps'>Box Jumps</option>
-                                <option name='kettlebellswings'>Kettle Bell Swings</option>
-                                <option name='sledpull'>Sled Pull</option>
-                                <option name='sledrows'>Sled Rows</option>
-                                <option name='sledlunge'>Sled Lunge</option>
-                                <option name='sledbearcrawl'>Sled Bear Crawl</option>
-                                {/* bodyweight */}
-                                <option name='bodyweight' disabled>BODYWEIGHT</option>
-                                <option name='airsquat'>Air Squat</option>
-                                <option name='burpees'>urpees</option>
-                                <option name='walkinglunges'>Walking Lunges</option>
-                                <option name='singlelegsquat'>Single Leg Squat</option>
-                                <option name='pushups'>Pushupst</option>
-                                <option name='lateralsquat'>Lateral Squat</option>
-                                <option name='staticlunge'>Static Lunge</option>
-                                <option name='singlelegdeadlift'>Single Leg Deadlift</option>
-                                <option name='scissorkicks'>Scissor Kicks</option>
-                                <option name='bulgariansplitsquat'>Bulgarian Split Squat</option>
-                                <option name='singlelegboxsquat'>Single Leg Box Squat</option>
-                            </SelectInput>
-
+                            />
                         </FormContainer>
                         <FormContainer>
                             <LabelStyle>Number of sets</LabelStyle>
@@ -426,11 +312,11 @@ const NewPlan = (props) => {
                         )
                     })}
                     <div>
-                        <ButtonStyle type='submit'>Create plan</ButtonStyle>
+                        <ButtonStyle type='submit'>Update plan</ButtonStyle>
                     </div>
                 </div>
             </form>
         </div >
     );
 }
-export default NewPlan;
+export default EditWorkoutPlan;
