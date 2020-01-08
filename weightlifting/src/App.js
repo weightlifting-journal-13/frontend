cef8444c9801c1758f2d8bfa207b9d97199fdad8
toddmurphy <toddmurphy93@gmail.com>
2020-01-08 15:28:14 -0400

    added logout, added more private routes, fixed navigation from the login, added navigation styling

--- weightlifting/src/App.js
+++ weightlifting/src/App.js
@@ -15,30 +15,16 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <Navigation />
-      <Route
-        exact path='/Registration'
-        render={props => <Registration {...props} />} />
-      <Route
-        exact path='/'
-        render={props => <Login {...props} />} />
+      {/* <Navigation /> */}
+      <PrivateRoute path='/' component={Navigation} />
+      <Route exact path='/Registration' render={props => <Registration {...props} />} />
+      <Route exact path='/' render={props => <Login {...props} />} />
       <PrivateRoute exact path='/Dashboard' component={Dashboard} />
-      <Route
-        exact path='/MyPlans'
-        render={props => <MyPlans {...props} />} />
-      <Route
-        exact path='/CreatePlan'
-        render={props => <CreatePlan {...props} />} />
-      <Route
-        exact path='/PresetPlans'
-        render={props => <PresetPlans {...props} />} />
-      <Route
-        exact path='/Profiles'
-        render={props => <Profiles {...props} />} />
-      <Route
-        exact path={`/EditPlan/:id`}
-        render={props => <EditWorkoutPlan {...props} />}
-      />
+      <PrivateRoute exact path='/MyPlans' component={MyPlans} />
+      <PrivateRoute exact path='/CreatePlan' component={CreatePlan} />
+      <PrivateRoute exact path='/PresetPlans' component={PresetPlans} />
+      <PrivateRoute exact path='/Profiles' component={Profiles} />
+      <PrivateRoute exact path='/EditPlan/:id' component={EditWorkoutPlan} />
     </div>
   );
 }
--- weightlifting/src/components/Navigation.js
+++ weightlifting/src/components/Navigation.js
@@ -1,24 +1,60 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
+import styled from 'styled-components';
+
+
+const NavigationWrapper = styled.div`
+    display: flex;
+    justify-content: center;
+    padding: 2% 0;
+    background: #00aced;
+
+    .selected{
+        text-decoration: underline;
+    }
+`
+
+const LogoutButton = styled.button`
+    width: 10%;
+    height: 40px;
+    margin: auto;
+    border-radius: 5px;
+    border: none;
+    font-size: 1rem;
+    font-weight: bold;
+    background: #01acee;
+    color: #FFF;
+    margin-left: 5%;
+    border: 2px solid #FFF;
+    
+`
+
+const LinkStyle = styled(NavLink).attrs(() => ({ activeClassName: 'selected' }))`
+    text-decoration: none;
+    color: #FFF;
+    padding:0 3%;
+    font-size: 1.4rem;
+    font-weight: bold;
+`
 
 const Navigation = (props) => {
 
-    // const logout = (event) => {
-    //     event.preventDefault();
+    const logout = (event) => {
+        event.preventDefault();
 
-    //     localStorage.clear();
-    //     props.history.push('/')
-    // }
+        localStorage.removeItem('token')
+        props.history.push('/')
+    }
 
     return (
-        <div>
-            <Link to={`/Dashboard`}>Dashboard</Link>
-            <Link to={`/MyPlans`}>My Plans</Link>
-            <Link to={`/CreatePlan`}>Create plan</Link>
-            <Link to={`/PresetPlans`}>Preset plans</Link>
-            <Link to={`/Profiles`}>Profiles</Link>
-            {/* <button onClick={logout} >Logout</button> */}
-        </div>
+        <NavigationWrapper>
+            <LinkStyle to={`/Dashboard`}>Dashboard</LinkStyle>
+            <LinkStyle to={`/MyPlans`}>My Plans</LinkStyle>
+            <LinkStyle to={`/CreatePlan`}>Create plan</LinkStyle>
+            <LinkStyle to={`/PresetPlans`}>Preset plans</LinkStyle>
+            <LinkStyle to={`/Profiles`}>Profiles</LinkStyle>
+            <LogoutButton onClick={logout} >Logout</LogoutButton>
+        </NavigationWrapper>
     );
 }
 
