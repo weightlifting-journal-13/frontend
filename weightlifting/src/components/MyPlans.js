c04ae35ebabe8528db912ce5243ceda53623f727
toddmurphy <toddmurphy93@gmail.com>
2020-01-07 20:03:54 -0400

    cleaned up Dashboardc omponent and moved that to MyPlans

--- weightlifting/src/components/MyPlans.js
+++ weightlifting/src/components/MyPlans.js
@@ -1,11 +1,19 @@
 import React from 'react';
 
-const MyPlans = () => {
-    return ( 
+const MyPlans = (props) => {
+
+    const editWorkoutPlan = (event) => {
+        event.preventDefault();
+
+        props.history.push(`/EditPlan/id`)
+    }
+
+    return (
         <div>
             <h1>My plans --> show newly create plans here</h1>
+            <button onClick={editWorkoutPlan}>Edit plan</button>
         </div>
-     );
+    );
 }
- 
+
 export default MyPlans;
