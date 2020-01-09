8b9b2107204a29b1663276b98c3a2ee5e11d1e9f
Dournbrood <tanner1220002@gmail.com>
2020-01-09 12:47:18 -0500

    reducer, store and global state cascade

--- weightlifting/src/index.js
+++ weightlifting/src/index.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {BrowserRouter as Router} from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import './index.css';
 import App from './App';
+import { WorkoutProvider } from "./reducers/WorkoutReducer";
 
 
 ReactDOM.render(
-<Router>
-    <App />
-</Router>,
-document.getElementById('root'));
+    <WorkoutProvider>
+        <Router>
+            <App />
+        </Router>
+    </WorkoutProvider>,
+    document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
--- weightlifting/src/reducers/WorkoutReducer.js
+++ weightlifting/src/reducers/WorkoutReducer.js
@@ -1,3 +1,14 @@
+import React, { createContext, useReducer } from "react";
+
+//Both of these imports are necessary to use global state in a component.
+//I know, we already import from react, but this should stay here for reference.
+// import { useContext } from 'react';
+// import { store } from "../reducers/WorkoutReducer";
+
+//To actually USE your values and magical dispatcher, you'd want to do: 
+// const globalState = useContext(store).state;
+// const {dispatch} = useContext(store);
+
 export const initialState = [
     {
         workout_name: "",
@@ -15,75 +26,95 @@ export const initialState = [
     }
 ]
 
-export const workoutReducer = (state = initialState, action) => {
-    console.log("STATE AND ACTION: ", state, action);
-    switch (action.type) {
-        case "REQUEST":
-            return ({
-                ...state,
-                busy: true,
-            });
+const store = createContext(initialState);
 
-        case "REQUEST_FAILED":
-            return ({
-                ...state,
-                busy: false,
-                error: action.payload,
-            });
+const { Provider } = store;
 
-        case "EDIT_EXERCISE_SUCCESS":
-            return ({
-                ...state,
-                busy: false,
-                error: ''
-            });
+const WorkoutProvider = ({ children }) => {
 
-        case "ADD_EXERCISE_SUCCESS":
-            return ({
-                ...state,
-                busy: false,
-                error: ''
-            });
-        case "DELETE_EXERCISE_SUCCESS":
-            return ({
-                ...state,
-                busy: false,
-                error: ''
-            });
-        case "ADD_WORKOUT_SUCCESS":
-            return ({
-                ...state,
-                busy: false,
-                error: ''
-            });
-        case "EDIT_WORKOUT_SUCCESS":
-            return ({
-                ...state,
-                busy: false,
-                error: ''
-            });
-        case "DELETE_WORKOUT_SUCCESS":
-            return ({
-                ...state,
-                busy: false,
-                error: ''
-            });
-        case "SELECT_WORKOUT_SUCCESS":
-            return ({
-                ...state,
-                busy: false,
-                error: ''
-            });
-        case "LOGIN_SUCCESS":
-            return ({
-                ...state,
-                // Storing our login token in state is PROBABLY NOT THE BEST IDEA, but localStorage is a rabbit hole for right now...
-                token: action.payload.token,
-                userID: action.payload.userID,
-                busy: false,
-                error: ''
-            });
-        default:
-            return state;
-    }
+    const [state, dispatch] = useReducer((state, action) => {
+        console.log("STATE AND ACTION: ", state, action);
+        switch (action.type) {
+            case "REQUEST":
+                return ({
+                    ...state,
+                    busy: true,
+                });
+
+            case "REQUEST_FAILED":
+                return ({
+                    ...state,
+                    busy: false,
+                    error: action.payload,
+                });
+
+            case "EDIT_EXERCISE_SUCCESS":
+                return ({
+                    ...state,
+                    busy: false,
+                    error: ''
+                });
+
+            case "ADD_EXERCISE_SUCCESS":
+                return ({
+                    ...state,
+                    busy: false,
+                    error: ''
+                });
+            case "DELETE_EXERCISE_SUCCESS":
+                return ({
+                    ...state,
+                    busy: false,
+                    error: ''
+                });
+            case "ADD_WORKOUT_SUCCESS":
+                return ({
+                    ...state,
+                    busy: false,
+                    error: ''
+                });
+            case "EDIT_WORKOUT_SUCCESS":
+                return ({
+                    ...state,
+                    busy: false,
+                    error: ''
+                });
+            case "DELETE_WORKOUT_SUCCESS":
+                return ({
+                    ...state,
+                    busy: false,
+                    error: ''
+                });
+            case "SELECT_WORKOUT_SUCCESS":
+                return ({
+                    ...state,
+                    busy: false,
+                    error: ''
+                });
+            case "LOGIN_SUCCESS":
+                localStorage.setItem('token', action.payload.token);
+                localStorage.setItem('user_id', action.payload.user_id);
+
+                console.log("GLOBAL STATE AFTER LOGIN IS: ", {
+                    ...state,
+                    // Storing our login token in state is PROBABLY NOT THE BEST IDEA, but localStorage is a rabbit hole for right now...
+                    user_id: action.payload,
+                    busy: false,
+                    error: ''
+                });
+                return ({
+                    ...state,
+                    // Storing our login token in state is PROBABLY NOT THE BEST IDEA, but localStorage is a rabbit hole for right now...
+                    user_id: action.payload,
+                    busy: false,
+                    error: ''
+                });
+            default:
+                return state;
+        };
+    }, initialState);
+
+    return (<Provider value={{ state, dispatch }}>{children}</Provider>);
 }
+
+export { store, WorkoutProvider }
