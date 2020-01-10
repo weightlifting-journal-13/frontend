261babcf9073eea3f8c2cad91ca2d7431a32f0a8
toddmurphy <toddmurphy93@gmail.com>
2020-01-09 22:14:28 -0400

    did master pull then re commited

--- weightlifting/src/components/MyPlans.js
+++ weightlifting/src/components/MyPlans.js
@@ -34,8 +34,8 @@ const MyPlans = (props) => {
         //     .delete(`/workouts/${id}`) //check for correct api endpoint -->
         //         console.log(response)
 
-    // const deleteWorkoutPlan = (event) => {
-    //     event.preventDefault();
+        // const deleteWorkoutPlan = (event) => {
+        //     event.preventDefault();
 
 
         //         props.history.push('/Dashboard')
@@ -48,23 +48,14 @@ const MyPlans = (props) => {
     return (
         <div>
 
-<<<<<<< HEAD
-
-
-
-            <h1>My plans --> show newly create plans here</h1>
-            <button onClick={editWorkoutPlan}>Edit plan</button>
-            <button onClick={deleteWorkoutPlan} >Delete plan</button>
-=======
             <h1> Your Created Plans</h1>
             {bodyPart.map((obj, index) => (
-              <MyPlanCard 
-                key={index}
-                bodypart={obj.bodypart}
-              />
+                <MyPlanCard
+                    key={index}
+                    bodypart={obj.bodypart}
+                />
             ))}
 
->>>>>>> eb019df025f5796b30934a794736b326cb7888af
         </div>
     );
 
